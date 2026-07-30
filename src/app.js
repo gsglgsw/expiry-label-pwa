af26709288982a664bf21128ca2d2a96676c08e3
gsglgsw <gsglgsw@gmail.com>
2026-07-30 17:36:00 +0800

    修正BUG

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ async function initApp() {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(reg => {
                 console.log('✅ [PWA] Service Worker 註冊成功，Scope:', reg.scope);
 
